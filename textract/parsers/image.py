8645e8732c424e88b9ec716c62e4ae0ff470a7c1
Patrick McClory <patrick@mcclory.io>
2020-11-01 09:29:06 -0800

    updating to allow textract to call proper tesseract binary via os env variable

--- textract/parsers/image.py
+++ textract/parsers/image.py
@@ -6,16 +6,20 @@ import os
 from .utils import ShellParser
 
 
+def get_tesseract_path():
+    return os.getenv("TEXTRACT_TESSERACT_PATH", "tesseract")
+
+
 class Parser(ShellParser):
     """Extract text from various image file formats using tesseract-ocr"""
 
     def extract(self, filename, **kwargs):
 
         # if language given as argument, specify language for tesseract to use
-        if 'language' in kwargs:
-            args = ['tesseract', filename, 'stdout', '-l', kwargs['language']]
+        if "language" in kwargs:
+            args = [get_tesseract_path(), filename, "stdout", "-l", kwargs["language"]]
         else:
-            args = ['tesseract', filename, 'stdout']
+            args = [get_tesseract_path(), filename, "stdout"]
 
         stdout, _ = self.run(args)
         return stdout
